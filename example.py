2ed76a8345a6fc287084d6cab19c09b7a67232bf
David Murray <dmurray@exo.quebec>
2020-08-11 10:32:57 -0400

    PEP8

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ from transport import RTPStream
 
 log = logging.getLogger('streamer')
 log_format = '[%(asctime)s] [%(name)s] [%(levelname)s] : %(message)s'
-logging.basicConfig(level=logging.INFO,format=log_format)
+logging.basicConfig(level=logging.INFO, format=log_format)
 
 
 streams = {
@@ -31,13 +31,13 @@ def save(stream):
         client.play()
 
         log.info(f'Saving chunks for <{name}>')
-        with open(f'/tmp/{name}','wb') as f:
+        with open(f'/tmp/{name}', 'wb') as f:
             for chunk in stream.generate():
                 f.write(chunk)
 
 def main():
     pool = Pool(len(streams))
-    pool.map(save,streams.items())
+    pool.map(save, streams.items())
 
 
 if __name__ == '__main__':
